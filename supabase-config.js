75fcd2a5daa639ad07551535233d3d6d35eb2d9f
tomonoriwaki <noriwakitomo@gmail.com>
2026-05-16 04:13:03 +0900

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.MOGU_SUPABASE_CONFIG = {
-  url: "",
-  anonKey: "",
+  url: "https://stbfukphwdvsabbqalal.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0YmZ1a3Bod2R2c2FiYnFhbGFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NjIzNzIsImV4cCI6MjA5NDQzODM3Mn0.K5irN-uN5UG3zUBn7GY_23rQuz8YQ0Uim1lFxt1K66o",
   storageBucket: "post-images",
 };
